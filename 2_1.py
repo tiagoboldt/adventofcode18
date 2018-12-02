5f84bed423b42cb47648c603a6d9bf1a88d2a213
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2018-12-02 15:18:40 +0000

    problem 2_1

---
+++ 2_1.py
@@ -0,0 +1,18 @@
+
+"""
+identifies which set of strings have a letter two or three times and multiples their count
+"""
+two = 0
+three = 0
+with open('2.input') as f:
+    for line in f:
+        foundThree = False
+        foundTwo = False
+        for c in range(ord('a'), ord('z')):
+            if not foundThree and line.count(chr(c)) == 3:
+                three += 1
+                foundThree = True
+            elif not foundTwo and line.count(chr(c)) == 2:
+                two += 1
+                foundTwo = True
+    print(three * two)
