8b82d2b723c38c7a46b73043e04a0634d9c0691a
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2018-12-03 20:06:13 +0000

    move regex line parse to config constructor

--- 3/3_1.py
+++ 3/3_1.py
@@ -8,27 +8,18 @@ SIZE = 1000
 fabric = [[0 for x in range(SIZE)] for y in range(SIZE)]
 reused_area = 0
 class Config():
-    ID = None
-    left = None
-    top = None
-    width = None
-    height = None
-
-def read_config(line):
-    regex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
-    match = regex.search(line)
-    result = Config()
-    result.ID = int(match.group(1))
-    result.left = int(match.group(2))
-    result.top = int(match.group(3))
-    result.width = int(match.group(4))
-    result.height = int(match.group(5))
-    return result
-
+    def __init__(self, line):
+        regex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+        match = regex.search(line)
+        self.ID = int(match.group(1))
+        self.left = int(match.group(2))
+        self.top = int(match.group(3))
+        self.width = int(match.group(4))
+        self.height = int(match.group(5))
 
 with open('3.input') as f:
     for line in f:
-        config = read_config(line)
+        config = Config(line)
         for i in range(config.top, config.top+config.height):
             for j in range(config.left, config.left+config.width):
                 if fabric[i][j] == 0:
